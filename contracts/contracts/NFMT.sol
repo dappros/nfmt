ecb9a068918003d12ecf1c064337cb40fc189f22
Taras Filatov <phwizard@gmail.com>
2022-05-31 16:39:05 +0100

    Update NFMT.sol

--- contracts/contracts/NFMT.sol
+++ contracts/contracts/NFMT.sol
@@ -59,10 +59,11 @@ contract NFMT is ERC1155, Ownable, Pausable, ERC1155Supply, ReentrancyGuard {
     // 🇬🇧🇺🇸: !!IMPORTANT!! - replace these addresses with ETH addresses of charities / volunteers whom you want to support!
 
     address constant CHARITY1 = 0x12c546c89E4158b151597d761048A5C33C4f54CC; // example N1 - 0x12c546c89E4158b151597d761048A5C33C4f54CC - "NOVA Ukraine", https://novaukraine.org/donate/
-    address constant CHARITY2 = 0xba65e794d8aec422d0d71cce1095c63da579e144; // example N2 - 0xba65e794d8aec422d0d71cce1095c63da579e144 - "GO Solidarna Sprava Gromad", Yurii Biryukov https://www.facebook.com/yuri.biriukov 
-    address constant CHARITY3 = 0xaD7F901b1eba489552c37b071b7D97DDB2DF27B6; // example N3 - 0xaD7F901b1eba489552c37b071b7D97DDB2DF27B6 - Demokratichna Sokyra (D7), https://sokyra.space/uk#help
+    address constant CHARITY2 = 0x8A4b9c5646A580d13E95164b7445E833bFcC78CF; // example N2 - 0xba65e794d8aec422d0d71cce1095c63da579e144 - "Mir & Co" fund, Myroslav Gai, https://mirco.com.ua/uk/requisites.html
+    address constant CHARITY3 = 0xaD7F901b1eba489552c37b071b7D97DDB2DF27B6; // example N3 - 0xaD7F901b1eba489552c37b071b7D97DDB2DF27B6 - Demokratychna Sokyra (D7), https://sokyra.space/uk#help
 
     // some other addresses you might consider:
+    // address constant CHARITY1 = 0xba65e794d8aec422d0d71cce1095c63da579e144; // "GO Solidarna Sprava Gromad", Yurii Biryukov https://www.facebook.com/yuri.biriukov 
     // address constant CHARITY1 = 02x93Bda139023d582C19D70F55561f23D3CA6a54c; // "Save Trempel", Kharkiv IT Cluster and Pavel Obod initiative - https://twitter.com/SaveTrempel
     // address constant CHARITY1 = 02x93Bda139023d582C19D70F55561f23D3CA6a54c; // "Come back alive" - https://www.comebackalive.in.ua/donate
     // address constant CHARITY1 = 0xA4166BC4Be559b762B346CB4AAad3b051E584E39; // "RazomForUkrane" - https://razomforukraine.org/donate/
